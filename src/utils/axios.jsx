5fc794d0acd124ca0a86db8a31e6713a34cc5c5a
Shubham <shubhamm.0010@gmail.com>
2025-05-10 17:11:12 +0530

    changess

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTkwODdiOGM4NTUzYTQ1NDk0YTcwNGM0ZjA5NTJlMCIsIm5iZiI6MTcxNTAyOTUwNi4yNDUsInN1YiI6IjY2Mzk0NjAyNWFkNzZiMDEyYzA1ZWFjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZdgY8Xsf78HgZL9Ah2tqG27dXwAgkS2KrgpGeVl_11I',
+    Authorization: ' Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNTkwODdiOGM4NTUzYTQ1NDk0YTcwNGM0ZjA5NTJlMCIsIm5iZiI6MTcxNTAyOTUwNi4yNDUsInN1YiI6IjY2Mzk0NjAyNWFkNzZiMDEyYzA1ZWFjNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZdgY8Xsf78HgZL9Ah2tqG27dXwAgkS2KrgpGeVl_11I',
     Accept: "application/json"
   },
 });
